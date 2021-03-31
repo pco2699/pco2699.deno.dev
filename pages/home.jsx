1b43ac324ae9491c035a2c2fc423ea8b55c604d4
pco2699 <aenkun@gmail.com>
2021-03-31 06:20:35 +0000

    initial commit

---
+++ pages/home.jsx
@@ -0,0 +1,11 @@
+import { h } from "https://deno.land/x/sift@0.1.6/mod.ts";
+import Layout from "../components/layout.jsx";
+
+export default async function homePage(request) {
+
+  return (<Layout>
+    <div className="container mx-auto max-w-screen-md p-4">
+        Hello World!
+    </div>
+  </Layout>);
+}
