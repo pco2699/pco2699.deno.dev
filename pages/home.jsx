c80a1271a49915ba9cfb611756db203aaf0c6aee
pco2699 <aenkun@gmail.com>
2021-04-11 17:19:47 +0900

    fix: home.jsx

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,11 +1,13 @@
 import { h } from "https://deno.land/x/sift@0.1.6/mod.ts";
+import dayjs from "https://deno.land/x/dayjs@1.10.4"
 import Layout from "../components/layout.jsx";
 
 export default async function homePage(request) {
 
-  return (<Layout>
+  return (
+  <Layout>
     <div className="container mx-auto max-w-screen-md p-4">
-        Hello World from pco2699!
+        {dayjs()}
     </div>
   </Layout>);
 }
