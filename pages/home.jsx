68527152c401af772b069e20fc347216c7e9d0b4
pco2699 <aenkun@gmail.com>
2021-04-11 17:53:21 +0900

    fix: home.jsx

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,13 +1,17 @@
 import { h } from "https://deno.land/x/sift@0.1.6/mod.ts";
-import dayjs from "https://cdn.skypack.dev/dayjs@1.10.4"
+import dayjs from "https://cdn.skypack.dev/dayjs@1.10.4";
+import relativeTime from "https://cdn.skypack.dev/dayjs@1.10.4/plugin/relativeTime";
+import objectSupport from "https://cdn.skypack.dev/dayjs@1.10.4/plugin/objectSupport";
 import Layout from "../components/layout.jsx";
 
 export default async function homePage(request) {
+  dayjs.extend(relativeTime);
+  dayjs.extend(objectSupport);
 
   return (
   <Layout>
     <div className="container mx-auto max-w-screen-md p-4">
-        {dayjs().format()}
+        {dayjs({ hour:24, minute:59 }).fromNow().format()}
     </div>
   </Layout>);
 }
